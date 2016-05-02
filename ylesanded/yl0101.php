c30f75ca04b5a25c5a1b6020664392f9b8a6589c
kerakaru <kerakaru@server.fake>
2016-05-02 11:02:10 +0300

    0104

--- ylesanded/yl0101.php
+++ ylesanded/yl0101.php
@@ -4,7 +4,7 @@
 echo "<h1>Ülesanne 0101</h1>";
 
 echo "<h2>Using mod</h2>";
-for ($i = 1; $i <20; $i++)
+for ($i = 1; $i <=19; $i++)
 {
     if ( ($i % 2) == 1 )
     {
@@ -14,7 +14,7 @@ for ($i = 1; $i <20; $i++)
 
 echo "<hr />";
 echo "<h2>Using for loop with step 2</h2>";
-for ($i = 1; $i <20; $i+=2)
+for ($i = 1; $i <=19; $i+=2)
 {
     echo $i . "<br />";
 }
