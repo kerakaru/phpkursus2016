e239727827b603ab9df240a409b4b7ed05f535d9
juku@tmk.edu.ee <juku@tmk.edu.ee>
2016-05-02 18:28:46 +0300

    array learning update

--- syntax/array.php
+++ syntax/array.php
@@ -9,14 +9,18 @@ $drinks[]="Jim Kiir";
 $drinks[]="Kuulus Teder";
 $drinks[]="keefir";
 
+echo $newLine . "<h2>üks väärtus</h2>";
 echo $newLine . $drinks[3];
 
+echo $newLine . $newLine ."<h2>var_dump()</h2>";
+echo "<pre>";
 var_dump($drinks);
+echo "</pre>";
+
+
 
 $rand = rand(0, sizeof($drinks)-1);
-
-
-
+echo $newLine . $newLine . "<h2>random väärtus</h2>";
 echo $newLine . $drinks[$rand];
 
 ?>
