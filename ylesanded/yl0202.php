967c6f4e58b9dcaa860050d1ec66a3df87665c09
juku@tmk.edu.ee <juku@tmk.edu.ee>
2016-05-09 18:13:47 +0300

    yl0202

---
+++ ylesanded/yl0202.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "<h1>Ülesanne 0202</h1>";
+
+$tmp_array = array();
+
+$animals[]="karu";
+$animals[]="rebane";
+$animals[]="hunt";
+$animals[]="siil";
+$animals[]="hirv";
+$animals[]="jänes";
+$animals[]="korall kaamel";
+$animals[]="põder";
+$animals[]="kährik";
+$animals[]="konn";
+
+
+#foreach tükliga väljastus
+foreach ($animals as $key => $val)
+{
+  echo "Massiivi võti on $key ja elemendi väärtus on $val<br />";
+  
+
+} 
+
+
+
+
+?>
